3b1578fb4c76d6eb9f79b4446b47bde8121b55c8
Bad Hard <badhard5693@gmail.com>
2022-02-28 20:56:08 +0300

    закончен dynamic

--- PR2_Var14/PR2_Var14.cpp
+++ PR2_Var14/PR2_Var14.cpp
@@ -84,47 +84,147 @@ double max_dist(int mas[][COLS]) {
 	return (round(max_d * 1000) / 1000);
 }
 
-void static_mas() {
-	int static_mas[ROWS][COLS];
-	int n_points;
-	zero_array(static_mas, ROWS, COLS);
+int enter_n_points() {
+	int n;
 	cout << "Сколько точек будет находиться на координатной плоскости?\n";
 	cout << "(Не больше 100)\n";
 	cout << "Ввод: ";
-	cin >> n_points;
+	cin >> n;
+	return n;
+}
+
+void static_mas() {
+	int static_mas[ROWS][COLS];
+	int n_points;
+	n_points = enter_n_points();
+	zero_array(static_mas, ROWS, COLS);
 	enter_points_coords(static_mas, n_points);
 	//print_array(static_mas);
 	cout << "Максимальное расстояние между заданными точками равно: ";
 	cout << max_dist(static_mas);
 }
 
-void create_dynamic_array(int**& mas) {
-	mas = new int* [COLS];
-	for (int i = 0; i < COLS; i++)
+void create_dynamic_array(int**& mas, int n, int m) {
+	mas = new int* [m];
+	for (int i = 0; i < m; i++)
 	{
-		mas[i] = new int[ROWS];
+		mas[i] = new int[n];
 	}
 }
 
-void zero_dynamic_array(int**& mas) {
-	for (int i = 0; i < COLS; i++)
-		for (int j = 0; j < ROWS; j++)
-			mas[i][j] = 0;
+void zero_dynamic_array(int**& mas, int n, int m) {
+	for (int i = 0; i < (n*2+1); i++)
+		for (int j = 0; j < (m*2+1); j++)
+			if (mas[i][j]!=1)
+				mas[i][j] = 0;
 }
 
-void print_dynamic_array(int** mas) {
-	for (int i = 0; i < COLS; i++) {
-		for (int j = 0; j < ROWS; j++)
+void print_dynamic_array(int** mas, int n, int m) {
+	for (int i = 0; i < (n*2+1); i++) {
+		for (int j = 0; j < (m*2+1); j++)
 			cout << mas[i][j] << " ";
 		cout << endl;
 	}
 }
 
+int enter_points_coords_dynamic(int**& mas, int n_points, int& n, int& m, int**& newMas) {
+	int x = 0;
+	int y = 0;
+
+	for (int i = 1; i <= n_points; i++)
+	{
+		cout << "Введите координаты " << i << "й точки (x,y): ";
+		cin >> x >> y;
+		if (abs(x)>n or abs(y)>m) {
+			if (abs(x) > n) {
+				/*newMas = (int**)realloc(newMas, (x * 2 + 1) * sizeof(int*));
+				for (int i = 0; i < (x * 2 + 1); i++)
+					newMas[i] = (int*)malloc((x * 2 + 1) * sizeof(int));*/
+				create_dynamic_array(newMas, abs(x) * 2 + 1, abs(x) * 2 + 1);
+				zero_dynamic_array(newMas, abs(x), abs(x));
+				for (int k = 0; k < (n*2+1); k++)
+				{
+					for (int r = 0; r < (m*2+1); r++)
+					{
+						newMas[k + abs(x) - n][r + abs(x) - n] = mas[k][r];
+					}
+				}
+				n = abs(x);
+				m = abs(x);
+				mas = newMas;
+			}
+			if (abs(y) > m) {
+				/*newMas = (int**)realloc(newMas, (y * 2 + 1) * sizeof(int*));
+				for (int i = 0; i < (y * 2 + 1); i++)
+					newMas[i] = (int*)malloc((y * 2 + 1) * sizeof(int));*/
+				create_dynamic_array(newMas, abs(y) * 2 + 1, abs(y) * 2 + 1);
+				zero_dynamic_array(newMas, abs(y), abs(y));
+				for (int k = 0; k < (n*2+1); k++)
+				{
+					for (int r = 0; r < (m*2+1); r++)
+					{
+						newMas[k + abs(y) - n][r + abs(y) - n] = mas[k][r];
+					}
+				}
+				n = abs(y);
+				m = abs(y);
+				mas = newMas;
+			}
+			
+			
+		}
+
+		if (mas[m - y][n + x] == 1) {
+			cout << "Точка с такими координатами уже существует." << endl;
+			i--;
+		}
+		else
+			mas[m - y][n + x] = 1;
+	}
+	return 0;
+}
+
+double max_dist_dynamic(int** mas, int n, int m) {
+	double max_d = -1;
+	int x, y;
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (mas[i][j] == 1)
+			{
+				for (int p = i; p < m; p++)
+				{
+					for (int k = 0; k < n; k++)
+					{
+						if (mas[p][k] == 1) {
+							double dist = sqrt(pow(double(p - i), 2.0) + pow(double(k - j), 2.0));
+							if (max_d < dist)
+								max_d = dist;
+						}
+					}
+				}
+			}
+		}
+	}
+	return (round(max_d * 1000) / 1000);
+}
+
+
 void dynamic_mas() {
 	int** dynamic_mas;
-	create_dynamic_array(dynamic_mas);
-	zero_dynamic_array(dynamic_mas);
-	print_dynamic_array(dynamic_mas);
+	int** newMas;
+	int n_points;
+	int n = (ROWS-1)/2;
+	int m = (COLS-1)/2;
+	create_dynamic_array(dynamic_mas, ROWS, COLS);
+	zero_dynamic_array(dynamic_mas,n,m);
+	n_points = enter_n_points();
+	//print_dynamic_array(dynamic_mas, n, m);
+	enter_points_coords_dynamic(dynamic_mas, n_points, n, m, newMas);
+	//print_dynamic_array(dynamic_mas, n, m);
+	cout << "Максимальное расстояние между заданными точками равно: ";
+	cout << max_dist_dynamic(dynamic_mas, n*2+1, m*2+1);
 }
 
 int main()
